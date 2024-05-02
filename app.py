845538ce64634df57bc3fa363f163367604a89b0
spiegelgo <167832064+spiegelgo@users.noreply.github.com>
2024-05-02 12:25:04 +0900

    한글 폰트 수정

--- app.py
+++ app.py
@@ -12,13 +12,20 @@ import numpy as np
 
 
 def main():
-    st.markdown("""
-    <style>
-        /* 나눔고딕 폰트 설정 */
-        .css-1vrbj2l {
-            font-family: 'NanumGothic', sans-serif !important;
-        }
-    </style>
+    # 한글 폰트 경로
+    font_path = "/usr/share/fonts/NanumGothic.ttf"  
+
+    # 한글 폰트 설정
+    st.markdown(f"""
+        <style>
+            @font-face {{
+                font-family: your_font;
+                src: url('data:font/truetype;charset=utf-8;base64,{font_path}') format('truetype');
+            }}
+            body {{
+                font-family: your_font;
+            }}
+        </style>
     """, unsafe_allow_html=True)
     st.title('K-Means 클러스터링 앱')
     st.text('csv 파일을 업로드 하면, 비슷한 유형의 데이터끼리 묶어주는 앱입니다.')
