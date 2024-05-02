c0b590eaf62ef66d79181727efac497bd578ef60
spiegelgo <167832064+spiegelgo@users.noreply.github.com>
2024-05-02 10:05:14 +0900

    version 1.0

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ from sklearn.compose import ColumnTransformer
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import font_manager, rc
+
+# 한글 폰트 설정
+font_path = "C:/Windows/Fonts/malgunsl.ttf"  # 한글 폰트 경로 
+font_name = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font_name)
 
 def main():
     st.title('K-Means 클러스터링 앱')
@@ -89,16 +95,43 @@ def main():
             st.dataframe( X_new )
 
             # 5. k의 갯수를 1개부터 10개까지 해서 wcss 를 구한다.
-            
+            wcss = []
+            for  k  in np.arange(1, 10+1):
+                kmeans = KMeans(n_clusters = k , random_state=5)
+                kmeans.fit(X_new)
+                wcss.append( kmeans.inertia_ )   
+                        
             # 6. elobw method 를 이용해서, 차트로 보여준다.
+            fig1 = plt.figure()
+            x = np.arange(1, 10+1)
+            plt.plot(x, wcss)
+            plt.title('엘보우 메소드')
+            plt.xlabel('클러스터의 갯수')
+            plt.ylabel('wcss')
+            st.pyplot(fig1)
+            
 
             # 7. 유저가 k의 갯수를 정한다.
-
+            k = st.slider('클러스터 갯수 설정', 1, 10)
+            
             # 8. KMeans 수행해서 그룹정보를 가져온다.
-
+            kmeans = KMeans(n_clusters = k , random_state=5)
+            y_pred = kmeans.fit_predict(X_new)
+            
             # 9. 원래 있던 df 에 Group 이라는 컬럼을 만들어준다. 
-
+            df['Group'] = y_pred
+                            
             # 10. 결과를 파일로 저장한다. 
-
+            df.to_csv('result.csv')
+            
+            # 11. 유저한테 보여준다
+            st.subheader('클러스터링 결과')
+            st.dataframe(df)
+            
+            # 12. 유저가 그룹을 선택하면 해당 그룹의 정보를 보여준다
+            choice = st.selectbox('그룹을 선택하세요',np.arange(0,k))
+            st.dataframe(df.loc[df['Group'] == choice, ])
+            
+                
 if __name__ == '__main__' :
     main()
