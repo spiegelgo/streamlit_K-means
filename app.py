26bcb5a0a1819bd454bb9d2cbd83ceb369d472ef
spiegelgo <167832064+spiegelgo@users.noreply.github.com>
2024-05-16 17:30:11 +0900

    텍스트 편집

--- app.py
+++ app.py
@@ -141,8 +141,8 @@ def main():
 
             csv = df.to_csv(index=False)
             b64 = base64.b64encode(csv.encode()).decode()
-            href = f'<a href="data:file/csv;base64,{b64}"</a>'
-            st.download_button(label="CSV 파일 저장", data=href, file_name="new_data.csv", mime="text/csv")    
+            href = f'<a href="data:file/csv;base64,{b64}">Download CSV 파일</a>'
+            st.markdown(href, unsafe_allow_html=True)    
                     
 if __name__ == '__main__' :
     main()
